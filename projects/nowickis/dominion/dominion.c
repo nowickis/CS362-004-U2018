1bdcd9a6a3b920137a15aa6a834b826b346995a3
Steven Nowicki <nowickis@flip3.engr.oregonstate.edu>
2018-07-13 16:17:32 -0700

    rest of unit tests for assignment-3

--- projects/nowickis/dominion/dominion.c
+++ projects/nowickis/dominion/dominion.c
@@ -743,7 +743,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   int index;
   int currentPlayer = whoseTurn(state);
   int nextPlayer = currentPlayer + 1;
-
+  int cardDrawn;
   int tributeRevealedCards[2] = {-1, -1};
   int temphand[MAX_HAND];// moved above the if statement
   int drawntreasure=0;
@@ -758,9 +758,29 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   switch( card ) 
     {
     case adventurer:
-      callAdventurer(drawntreasure, currentPlayer, state, temphand, z);
+      //callAdventurer(drawntreasure, currentPlayer, state, temphand, z);
+      
 			
-    case council_room:
+  	while(drawntreasure<2){
+	 if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
+	  shuffle(currentPlayer, state);
+	}
+	drawCard(currentPlayer, state);
+	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
+	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+	  drawntreasure++;
+	else{
+	  temphand[z]=cardDrawn;
+	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+	  z++;
+	}
+      }
+      while(z-1>=0){
+	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
+	z=z-1;
+      }
+      return 0;
+     case council_room:
       //+4 Cards
       for (i = 0; i < 4; i++)
 	{
@@ -967,7 +987,10 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case great_hall:
-      callGreatHall(currentPlayer, state, handPos);
+     drawCard(currentPlayer, state);
+     state->numActions++;
+     discardCard(handPos, currentPlayer, state, 0);
+     return 0;
 		
     case minion:
       //+1 action
@@ -1021,8 +1044,29 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case steward:
-      callSteward(choice1, choice2, choice3, currentPlayer, state, handPos);
-		
+    		
+     if (choice1 == 1)
+	{
+	  
+	  drawCard(currentPlayer, state);
+	  drawCard(currentPlayer, state);
+	}
+      else if (choice1 == 2)
+	{
+	  
+	  state->coins = state->coins + 2;
+	}
+      else
+	{
+	  
+	  discardCard(choice2, currentPlayer, state, 1);
+	  discardCard(choice3, currentPlayer, state, 1);
+	}
+			
+      
+      discardCard(handPos, currentPlayer, state, 0);
+      return 0;    
+    
     case tribute:
       if ((state->discardCount[nextPlayer] + state->deckCount[nextPlayer]) <= 1){
 	if (state->deckCount[nextPlayer] > 0){
--- projects/nowickis/dominion/dominion.h
+++ projects/nowickis/dominion/dominion.h
@@ -131,4 +131,6 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 int compare(const void* a, const void* b);
 
 int callSmithy(int handPos, int currentPlayer, struct gameState *state);
+
+int callAdventurer(int drawntreasure, int currentPlayer, struct gameState *state, int *temphand, int z);
 #endif
